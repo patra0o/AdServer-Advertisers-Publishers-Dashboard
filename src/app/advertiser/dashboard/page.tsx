433aaa11e536d97ac64abbe1ca70698b8556b05a
edilson zau <dannibuga@gmail.com>
2024-04-08 00:56:50 +0200

    Advertiser Layout created

--- src/app/advertiser/dashboard/page.tsx
+++ src/app/advertiser/dashboard/page.tsx
@@ -5,27 +5,6 @@ import React from 'react'
 
 export default function AdvertiserDashboard() {
     return (
-        <div className='advertiser-dashboard-parent'>
-            <div className='advertiser-sidebar'>
-                <img src='https://via.placeholder.com/90x90'></img>
-                <div className='advertiser-sidebar-nav'>
-                    <h2>Campaigns</h2>
-                    <h2>Inventory</h2>
-                    <h2>Reports</h2>
-                    <h2>Settings</h2>
-                </div>
-            </div>
-            <div className='advertiser-main-body'>
-                <div className='navbar'>
-                    <div></div>
-                    <div className='nav-user-details'>
-                        <h2>John Doe</h2>
-                        <img src='https://via.placeholder.com/50x50'></img>
-                        <div></div>
-                    </div>
-                </div><br />
-                <CampaignDataGrid></CampaignDataGrid>
-            </div>
-        </div>
+        <CampaignDataGrid></CampaignDataGrid>
     )
 }
