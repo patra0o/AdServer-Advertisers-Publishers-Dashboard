e08dc49155c651f4b986898ae3957d08f0a998f7
edilson zau <dannibuga@gmail.com>
2024-04-21 00:33:19 +0200

    Publisher dashboard layout

--- src/app/publisher/dashboard/page.tsx
+++ src/app/publisher/dashboard/page.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function PublisherDashboard() {
-  return (
-    <div>page</div>
-  )
+    return (
+        <div className='publisher-dashboard-parent'>
+            <div className='publisher-navbar'>
+                <img src='/dynamic-rockets-logo.png' height={70} width={70}></img>
+
+                <div className='dropdown'>
+                    <a href='#' style={{ textDecoration: 'none' }}><span>My account</span></a>
+                    <div className="dropdown-content">
+                        <Link href='/publisher/dashboard/my-account'>Profile</Link>
+                        <Link href='/'>Log-out</Link>
+                    </div>
+                </div>
+            </div>
+            <div className='publisher-main-body'>
+                <div className="publisher-main-body-nav">
+                    <h2>Dashboard</h2>
+                    <h2>Bids</h2>
+                </div>
+                <div className="highlightContainer">
+                    <div className="highlightItem">
+                        <h1><img src='/eye.png' height={40} width={40} style={{ margin: "0 10px" }} />34.1k</h1>
+                        <h3>Views</h3>
+                    </div>
+                    <div className="highlightItem">
+                        <h1><img src='/click.png' height={40} width={40} style={{ margin: "0 10px" }} />9.7k</h1><br />
+                        <h3>Clicks</h3>
+                    </div>
+                    <div className="highlightItem">
+                        <h1><img src='/bidding.png' height={40} width={40} style={{ margin: "0 10px" }} />15</h1>
+                        <h3>Bids</h3>
+                    </div>
+                    <div className="highlightItem">
+                        <h1><img src='/money.png' height={40} width={40} style={{ margin: "0 10px" }} />N$10.9K</h1>
+                        <h3>Income</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/app/publisher/page.tsx
+++ src/app/publisher/page.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import Link from 'next/link'
+"use client"
 
+import React, { FormEvent } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 export default function PublisherLogin() {
+  const router = useRouter()
+
+  // Function to authenticate the user and redirect to the Advertiser Dashboard
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevents default form behavior
+
+    router.push('/publisher/dashboard')
+  };
   return (
     <div className='home-page-parent'>
       <div className='home-page-container'>
         <h2>Publisher Login</h2>
-        <form className='login-form-input-holder'>
+        <form className='login-form-input-holder' onSubmit={handleSubmit}>
           <input className='login-form-input' type='email' placeholder='Enter your email'></input>
-          <input className='login-form-input' type='password' placeholder='Enter your password'></input><br/>
+          <input className='login-form-input' type='password' placeholder='Enter your password'></input><br />
           <button className='home-page-button'>Login</button>
         </form>
         <p style={{ paddingTop: 20 }}>Don't have an account? <Link href="/register">Register</Link></p>
